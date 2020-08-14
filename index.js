0615e905bf727e9055a7de83a96f9fb47b327737
pbedrosian <peter_bedrosian@Yahoo.com>
2020-08-13 21:10:51 -0400

    Done.

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ document.addEventListener( "DOMContentLoaded", function () {
     document.getElementById( "text" )
       .innerHTML = "This is really cool!";
   }
+
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
